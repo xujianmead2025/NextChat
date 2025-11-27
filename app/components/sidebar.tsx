5bb82664e66a658e89e15562522bbe1a1e77e849
xujian <xujianmead@gmail.com>
2025-11-27 18:02:53 +0800

    Change NextChat to Green World

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -250,8 +250,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="Green World"
+        subTitle="AI customer service!"
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
